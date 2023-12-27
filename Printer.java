bdeaa1647ee2e48ef0605f34d9093ece649258cf
Pankaj Singh <121671526+pankajkscs@users.noreply.github.com>
2023-12-27 23:50:11 +0530

    singleton class

---
+++ Printer.java
@@ -0,0 +1,21 @@
+package com.infy.java8streamapiex.singaltonclass;
+
+public class Printer {
+    // step-2
+    private static Printer INSTANCE;
+
+    // step-1
+    public Printer() {
+        System.out.println("No Arg Constructer");
+    }
+
+    // step-3
+    public static Printer getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Printer();
+            // return INSTANCE;
+        }
+        return INSTANCE;
+    }
+
+}
